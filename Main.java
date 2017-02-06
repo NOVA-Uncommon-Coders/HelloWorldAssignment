3dd95fff91dad879656deb27aff7cca5ca9e3c49
Alvaro Balboa <souporman@alvaros-mbp.nvstu.edu>
2017-02-06 14:09:21 -0500

    commitMessage

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        /*Scanner kb = new Scanner(System.in);
+        String name;
+        System.out.printf("Please type in your name?");
+        name = kb.nextLine();
 
+        if(name == "Slim Shady"){
+            System.out.printf("My name is what");
+        }
+        else
+            System.out.printf("My name is %s",name);
+        */
+        System.out.printf("Alvaro Balboa");
 
     }
 }
