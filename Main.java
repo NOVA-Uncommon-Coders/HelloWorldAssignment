901043fa431c00ecc26e5fdb3c62a47fc3ab598b
JacobP <JacobP@macusermacbook6.nvstu.edu>
2017-02-06 15:27:36 -0500

    added variable

--- Main.java
+++ Main.java
@@ -3,9 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-// Prints “JacobP” in the terminal window.
-        System.out.println("JacobP");
 
+        String myName = "Jacob";
+
+        System.out.println("JacobP");
 
     }
 }
+
